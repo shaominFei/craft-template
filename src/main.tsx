e7e9f2ccdb290f96269380f3446f5ff40f190fc2
Shaomin Fei <sfei@obsglobal.com>
2025-01-22 11:30:58 -0600

    import font

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { theme } from '@/config/theme.ts';
 import '@/services/i18n/i18n.ts';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
